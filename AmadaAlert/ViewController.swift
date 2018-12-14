68737cf911e1570f84232129c91a8b4163ab8f65
IT Support <itsupport@ITs-MacBook-Pro.local>
2018-12-14 14:00:01 -0800

    Initial Commit

---
+++ AmadaAlert/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AmadaAlert
+//
+//  Created by IT Support on 12/14/18.
+//  Copyright © 2018 IT Support. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
